b287931fa087cd1d3262e58d5bca3d2241e0febb
yiwei <88491304+yiweisin@users.noreply.github.com>
2024-05-23 14:52:47 +0800

    1

--- project1/minesweeper/minesweeper.py
+++ project1/minesweeper/minesweeper.py
@@ -105,13 +105,13 @@ class Sentence():
         """
         Returns the set of all cells in self.cells known to be mines.
         """
-        raise NotImplementedError
+       
 
     def known_safes(self):
         """
         Returns the set of all cells in self.cells known to be safe.
         """
-        raise NotImplementedError
+      
 
     def mark_mine(self, cell):
         """
