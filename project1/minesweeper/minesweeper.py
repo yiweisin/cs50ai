44250edf7dfc3d2176186450556874cf3c5c380b
yiwei <88491304+yiweisin@users.noreply.github.com>
2024-05-23 16:55:57 +0800

    2

--- project1/minesweeper/minesweeper.py
+++ project1/minesweeper/minesweeper.py
@@ -105,28 +105,32 @@ class Sentence():
         """
         Returns the set of all cells in self.cells known to be mines.
         """
+        
+
        
 
     def known_safes(self):
         """
         Returns the set of all cells in self.cells known to be safe.
         """
-      
+        
 
     def mark_mine(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be a mine.
         """
-        raise NotImplementedError
+        if cell in self.cells:
+            self.cells.remove(cell)
+            self.count -= 1
 
     def mark_safe(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be safe.
         """
-        raise NotImplementedError
-
+        if cell in self.cells:
+            self.cells.remove(cell)
 
 class MinesweeperAI():
     """
@@ -203,3 +207,4 @@ class MinesweeperAI():
             2) are not known to be mines
         """
         raise NotImplementedError
+
